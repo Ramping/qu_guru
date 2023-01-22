d6eaa1e2d0181d1801e9b451fac5e811354fe06e
Alexandr Popov <harvard33rus@gmail.com>
2023-01-22 21:42:04 +0300

    update quotes

--- test_lesson_6.py
+++ test_lesson_6.py
@@ -13,18 +13,18 @@ zip_path = os.path.join(resources_path, 'new_archive.zip')
 
 def test_create_archive():
     os.mkdir(resources_path)
-    with ZipFile(zip_path, "w") as zip_file:
+    with ZipFile(zip_path, 'w') as zip_file:
         for file in os.listdir(files_dir):
             add_file = os.path.join(files_dir, file)
             zip_file.write(add_file, basename(add_file))
     files = os.listdir(resources_path)
     assert len(files) == 1, 'one archive must be created'
-    assert "new_archive.zip" == files[0], 'filename should be new_archive.zip'
+    assert 'new_archive.zip' == files[0], 'filename should be new_archive.zip'
 
 
 def test_pdf():
     with ZipFile(zip_path) as zf:
-        pdf_file = zf.extract("docs-pytest-org-en-latest.pdf")
+        pdf_file = zf.extract('docs-pytest-org-en-latest.pdf')
         reader = PdfReader(pdf_file)
         page = reader.pages[0]
         assert 'pytest Documentation' in page.extract_text(), 'information in pdv file not found'
@@ -34,7 +34,7 @@ def test_pdf():
 
 def test_xlsx():
     zf = ZipFile(zip_path)
-    with zf.open("file_example_XLSX_50.xlsx") as xlsxfile:
+    with zf.open('file_example_XLSX_50.xlsx') as xlsxfile:
         xlsxfile = load_workbook(xlsxfile)
         sheet = xlsxfile.active
         value = sheet.cell(row=2, column=2).value
@@ -44,10 +44,10 @@ def test_xlsx():
 
 def test_csv():
     zf = ZipFile(zip_path)
-    with zf.open("username.csv") as csvfile:
+    with zf.open('username.csv') as csvfile:
         csvfile = csv.reader(TextIOWrapper(csvfile))
-        list_csv = ["".join(r).replace(";", " ", 3) for r in csvfile]
-    assert "booker12 9012 Rachel Booker" in list_csv, 'information in csv file not found'
+        list_csv = [''.join(r).replace(';', ' ', 3) for r in csvfile]
+    assert 'booker12 9012 Rachel Booker' in list_csv, 'information in csv file not found'
     zf.close()
     os.remove(os.path.join(resources_path, 'new_archive.zip'))
     os.rmdir(resources_path)
