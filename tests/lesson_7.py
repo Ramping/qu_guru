3ca0bd88dbc3a34ba46672e886d1e1875c6b5370
Alexandr Popov <harvard33rus@gmail.com>
2023-01-23 17:23:52 +0300

    add allure annotatoins

--- tests/lesson_7.py
+++ tests/lesson_7.py
@@ -1,3 +1,4 @@
+from allure_commons.types import Severity
 from selene.support import by
 from selene.support.conditions import be
 from selene.support.shared import browser
@@ -5,41 +6,59 @@ from selene.support.shared.jquery_style import s
 import allure
 
 
+@allure.tag('web')
+@allure.severity(Severity.NORMAL)
+@allure.label('owner', 'popov')
+@allure.feature('Tasks in the repository')
+@allure.story('Sample test without steps')
+@allure.link('https://github.com', name='Testing')
 def test_github():
     browser.open('https://github.com')
 
     s('.header-search-input').click()
-    s('.header-search-input').send_keys('eroshenkoam/allure-example')
+    s('.header-search-input').send_keys('Ramping/qu_guru')
     s('.header-search-input').submit()
 
-    s(by.link_text('eroshenkoam/allure-example')).click()
+    s(by.link_text('Ramping/qu_guru')).click()
 
     s('#issues-tab').click()
 
-    s(by.partial_text('#76')).should(be.visible)
+    s(by.partial_text('#1')).should(be.visible)
 
 
+@allure.tag('web')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'popov')
+@allure.feature('Tasks in the repository')
+@allure.story('Sample test with lambda steps')
+@allure.link('https://github.com', name='Testing')
 def test_dynamic_steps():
     with allure.step('Opening the main page'):
         browser.open('https://github.com')
     with allure.step('Looking for a repository'):
         s('.header-search-input').click()
-        s('.header-search-input').send_keys('eroshenkoam/allure-example')
+        s('.header-search-input').send_keys('Ramping/qu_guru')
         s('.header-search-input').submit()
     with allure.step('Go to repository link'):
-        s(by.link_text('eroshenkoam/allure-example')).click()
+        s(by.link_text('Ramping/qu_guru')).click()
     with allure.step('Open the Issues tab'):
         s('#issues-tab').click()
-    with allure.step('Checking for Issue number 76'):
+    with allure.step('Checking for Issue number 1'):
         s(by.partial_text('#76')).should(be.visible)
 
 
+@allure.tag('web')
+@allure.severity(Severity.BLOCKER)
+@allure.label('owner', 'popov')
+@allure.feature('Tasks in the repository')
+@allure.story('Decorator test example')
+@allure.link('https://github.com', name='Testing')
 def test_decorator_steps():
     open_main_page()
-    search_for_repository('eroshenkoam/allure-example')
-    go_to_repository('eroshenkoam/allure-example')
+    search_for_repository('Ramping/qu_guru')
+    go_to_repository('Ramping/qu_guru')
     open_issue_tab()
-    should_see_issue_with_number('#76')
+    should_see_issue_with_number('#1')
 
 
 @allure.step('Opening the main page')
@@ -64,6 +83,6 @@ def open_issue_tab():
     s("#issues-tab").click()
 
 
-@allure.step('Checking for Issue number 76')
+@allure.step('Checking for Issue number 1')
 def should_see_issue_with_number(number):
     s(by.partial_text(number)).click()
