e6c5c652a2939cd28ed42a76ba8774a72b63bfbc
Alexandr Popov <harvard33rus@gmail.com>
2023-01-24 13:04:46 +0300

    update conftest

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope="function", autouse=True)
+def app():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield browser
+    browser.quit()
