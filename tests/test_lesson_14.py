00b3d23dd3e8629f1d58b580e64d6eb20bea6b4e
Alexandr Popov <harvard33rus@gmail.com>
2023-03-15 18:01:17 +0300

    change reqres test

--- tests/test_lesson_14.py
+++ tests/test_lesson_14.py
@@ -1,12 +1,9 @@
-import requests
 from pytest_voluptuous import S
 from schemas.schemas import *
 
-base_url = "https://reqres.in/"
 
-
-def test_create_user():
-    created_user = requests.post(f"{base_url}api/users", {"name": "Alex", "job": "Tester"})
+def test_create_user(reqres):
+    created_user = reqres.post("api/users", {"name": "Alex", "job": "Tester"})
 
     assert created_user.status_code == 201
     assert S(create_user) == created_user.json()
@@ -14,8 +11,8 @@ def test_create_user():
     assert created_user.json()["job"] == "Tester"
 
 
-def test_update_user_by_put():
-    update_user = requests.put(f"{base_url}api/users/2", {"name": "apopov", "job": "aqa"})
+def test_update_user_by_put(reqres):
+    update_user = reqres.put("api/users/2", {"name": "apopov", "job": "aqa"})
 
     assert update_user.status_code == 200
     assert S(create_update_user) == update_user.json()
@@ -23,8 +20,8 @@ def test_update_user_by_put():
     assert update_user.json()["job"] == "aqa"
 
 
-def test_update_user_by_patch():
-    update_user = requests.put(f"{base_url}api/users/2", {"name": "Popov", "job": "sdet"})
+def test_update_user_by_patch(reqres):
+    update_user = reqres.put("api/users/2", {"name": "Popov", "job": "sdet"})
 
     assert update_user.status_code == 200
     assert S(create_update_user) == update_user.json()
@@ -32,14 +29,14 @@ def test_update_user_by_patch():
     assert update_user.json()["job"] == "sdet"
 
 
-def test_delete_user():
-    delete_user = requests.delete(f"{base_url}api/users/2")
+def test_delete_user(reqres):
+    delete_user = reqres.delete("api/users/2")
 
     assert delete_user.status_code == 204
 
 
-def test_successful_register():
-    user_register = requests.post(f"{base_url}api/register", {"email": "eve.holt@reqres.in", "password": "1234"})
+def test_successful_register(reqres):
+    user_register = reqres.post("api/register", {"email": "eve.holt@reqres.in", "password": "1234"})
 
     assert user_register.status_code == 200
     assert S(register_user) == user_register.json()
@@ -47,8 +44,8 @@ def test_successful_register():
     assert user_register.json()['token']
 
 
-def test_unsuccessful_register():
-    user_register = requests.post(f"{base_url}api/register", {"email": "5678@gmail.com"})
+def test_unsuccessful_register(reqres):
+    user_register = reqres.post("api/register", {"email": "5678@gmail.com"})
 
     assert user_register.status_code == 400
     assert S(unregister_user) == user_register.json()
